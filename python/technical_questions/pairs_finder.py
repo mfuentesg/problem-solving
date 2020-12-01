66b48baf5a2ffca040907e8cc434ba7b27cd96ad
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2020-12-01 13:34:41 -0300

    add base folder structure

--- python/technical_questions/pairs_finder.py
+++ python/technical_questions/pairs_finder.py
@@ -12,34 +12,37 @@ Assumptions:
 - A[i] is a positive number
 """
 
+
 # Solution 1 - Brute force => O(n^2)
 def brute_force(A, k):
     counter = 0
     for i in range(len(A)):
-        for j in range(i+1, len(A)):
+        for j in range(i + 1, len(A)):
             if abs(A[i] - A[j]) == k:
                 counter += 1
                 print('pair found ({},{})'.format(A[i], A[j]))
     return counter
 
+
 # Solution 2 - Optimal solution => O(n)
 def optimal_solution(A, k):
     counter = 0
-    numbers = {n:i for (i,n) in enumerate(A)}
+    numbers = {n: i for (i, n) in enumerate(A)}
 
     for n in A:
-        if n-k in numbers:
-            counter = counter+1
-            print('pair found ({},{})'.format(n, n-k))
+        if n - k in numbers:
+            counter = counter + 1
+            print('pair found ({},{})'.format(n, n - k))
 
-        if n+k in numbers:
-            counter = counter+1
-            print('pair found ({},{})'.format(n, n+k))
+        if n + k in numbers:
+            counter = counter + 1
+            print('pair found ({},{})'.format(n, n + k))
 
         numbers.pop(n)
 
     return counter
 
-A = [1,7,5,9,2,12,3]
-print('brute force pairs count: ', brute_force(A, 2))
-print('optimal solution pairs count: ', optimal_solution(A, 2))
+
+input_list = [1, 7, 5, 9, 2, 12, 3]
+print('brute force pairs count: ', brute_force(input_list, 2))
+print('optimal solution pairs count: ', optimal_solution(input_list, 2))
