2b9cb1e8225c9e290f034e16e50b43734c3d68ee
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2021-01-04 15:20:54 -0300

    finish day 4

--- daily-coding-problem/day_4.py
+++ daily-coding-problem/day_4.py
@@ -8,3 +8,22 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 
 You can modify the input array in-place.
 """
+
+
+def finder(elements):
+    items = dict()
+    for index, element in enumerate(elements):
+        if element < 1:
+            continue
+        items[element] = index
+
+    max_element = max(elements)  # O(n)
+    for i in range(1, max_element):
+        if items.get(i) is None:
+            return i
+    return max_element + 1
+
+
+assert finder([3, 4, -1, 1]) == 2
+assert finder([3, 4, 2, 1]) == 5
+assert finder([1, 2, 0]) == 3
