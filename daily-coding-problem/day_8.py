5e481c10c941d14ad14fe2bd98f0710e78621576
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2021-01-04 15:21:21 -0300

    solve unival tree excercise

--- daily-coding-problem/day_8.py
+++ daily-coding-problem/day_8.py
@@ -27,7 +27,20 @@ unival: 4
 
 
 def unival_count(node):
-    pass
+    if node is None:
+        return 0
+
+    if node.left is None and node.right is None:
+        return 1
+
+    if node.right is None and node.left.value == node.value:
+        return 1 + unival_count(node.left)
+
+    if node.left is None and node.right.value == node.value:
+        return 1 + unival_count(node.right)
+
+    extra = 1 if node.value == node.left.value and node.right.value == node.left.value else 0
+    return extra + unival_count(node.left) + unival_count(node.right)
 
 
 class Node:
@@ -41,4 +54,4 @@ bt1 = Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0)))
 bt2 = Node(1, right=Node(1, right=Node(1, right=Node(1))))
 
 assert unival_count(bt1) == 5
-assert unival_count(bt1) == 4
+assert unival_count(bt2) == 4
